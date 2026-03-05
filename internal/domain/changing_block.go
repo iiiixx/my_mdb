00e18f25e9d8086ac9415e39729f850599fa2bf7
анастасия <anastasia@MacBook-Air-anastasia-3.local>
2026-03-05 18:21:53 +0300

    change ChangingBlock for remove mapping

--- internal/domain/changing_block.go
+++ internal/domain/changing_block.go
@@ -1,6 +1,12 @@
 package domain
 
 type ChangingBlock struct {
+	Kind   string
+	Title  string
+	Movies []Movie
+}
+
+type ChangingBlockResponse struct {
 	Kind   string      `json:"kind"`
 	Title  string      `json:"title"`
 	Movies []MovieCard `json:"movies"`
