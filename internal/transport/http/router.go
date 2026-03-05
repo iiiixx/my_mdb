ce128aef9b51e2b164f61e87f9dc0b5f5bd80e95
анастасия <anastasia@MacBook-Air-anastasia-3.local>
2026-03-05 18:30:47 +0300

    feat: add handles

--- internal/transport/http/router.go
+++ internal/transport/http/router.go
@@ -3,6 +3,7 @@ package http
 import (
 	"net/http"
 
+	"my_mdb/internal/frontend"
 	"my_mdb/internal/service"
 	"my_mdb/internal/transport/http/handler"
 	mymw "my_mdb/internal/transport/http/mymiddleware"
@@ -11,8 +12,14 @@ import (
 	chimw "github.com/go-chi/chi/v5/middleware"
 )
 
-func NewRouter(app *service.App) http.Handler {
+func NewRouter(app *service.App, httpAddr string) http.Handler {
 	r := chi.NewRouter()
+	fe, err := frontend.New("http://localhost" + httpAddr)
+	if err != nil {
+		app.Log.WithError(err).Fatal("frontend failed to initialize")
+	}
+
+	app.Log.Info("frontend initialized successfully")
 
 	r.Use(chimw.RequestID)
 	r.Use(chimw.RealIP)
@@ -29,7 +36,7 @@ func NewRouter(app *service.App) http.Handler {
 	})
 	r.Route("/api", func(r chi.Router) {
 		r.Post("/users", ah.CreateUser)
-		//r.Get("/users/{userID}/validate", ah.ValidateUser)
+		r.Get("/users/{userID}/validate", ah.ValidateUser)
 
 		r.Get("/movies/search", mh.Search)
 		r.Get("/movies/top200", mh.Top200)
@@ -43,11 +50,13 @@ func NewRouter(app *service.App) http.Handler {
 			r.Get("/home", hh.Home)
 
 			r.Get("/watched", mh.WatchedByUser)
+			r.Get("/recommend", mh.Recommendation)
 			r.Get("/movies/{movieID}", mh.Details)
 
 			r.Put("/ratings/{movieID}", rh.Upsert)
 		})
 	})
+	r.Mount("/", fe.Routes())
 
 	return r
 }
